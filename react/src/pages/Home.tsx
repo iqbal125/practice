829f841c07edbd710e58e8d8ad6f9ac9c90a6e06
iqbal125 <iqbal125sf@gmail.com>
2025-09-12 07:58:43 -0700

    remove public

--- react/src/pages/Home.tsx
+++ react/src/pages/Home.tsx
@@ -7,7 +7,7 @@ const Home: React.FC = () => {
     return (
         <div className="max-w-xl mx-auto p-6 space-y-6">
             <nav className="bg-white shadow px-4 py-2 flex space-x-4">
-                <Link to="/link1" className="text-blue-600 hover:underline">
+                <Link to="/" className="text-blue-600 hover:underline">
                     Link 1
                 </Link>
             </nav>
---
+++ react/src/pages/SetTImeoutPage.tsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+
+const SetTimeoutPage: React.FC = () => {
+    const [counter, setCounter] = useState(0)
+
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default SetTimeoutPage;
