955181375c9b55cbaaf8ee4bfacf78ad29d0a4d9
iqbal125 <iqbal125sf@gmail.com>
2025-09-18 11:33:53 -0700

    update

---
+++ python/ValidParenthesis.py
@@ -0,0 +1,34 @@
+
+
+
+
+def ValidParen(s): 
+    stk = []
+    hashMap = {
+        ")": "(", "]": "[", "}": "{" 
+    }
+
+    for char in s:
+        # if closing bracket
+        if char in hashMap:
+            if(len(stk) == 0 or stk.pop() != hashMap[char]):
+                return False
+        # if opening bracket
+        else: 
+            stk.append(char)
+
+    if len(stk) == 0:
+        return True
+
+
+
+
+
+
+
+
+
+s1 = "[([])]" #true
+s2 = "[(])" #false
+
+print(ValidParen(s2))
